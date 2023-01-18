f0eabdc165cebc6e39c8d05195598d3626e81c90
Nicholas Anderson <nla2121@tc.columbia.edu>
2023-01-17 22:30:33 -0500

    Adding numpy functions

--- General Course Materials/Useful python functions.py
+++ General Course Materials/Useful python functions.py
@@ -52,6 +52,9 @@ x.replace("z", "sa") # replace z with sa
 # Check index of a particular character/string
 x.index("a")
 
+# Coerce numeric type to string to pring
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
 
 """ Importing Python Libraries or functions from libraries """ # =============================================================================
 
@@ -62,8 +65,32 @@ from math import radians
 
 """ numpy objects/methods/functions  """ # =============================================================================
 
-# Create a np.array
+# Create a np.array. A np.array can only contain one type of data, unlike a list which can 
+# contain many different data types. np.array are really designed for perofrming
+# element wise mathematical operations
 x = np.array([1, 2, 3])
+
+# slice array based on logical statement
+bmi[ bmi > 23 ]
+
+# See dimensions of np.array
+np_array.shape
+
+# compute mean, median, sd
+array.mean()
+np.mean(array)
+np.median(array)
+np.std(array)
+
+# Compute correlation between two columns
+corr = np.corrcoef(np_baseball[:,0], np_baseball[:,1])
+
+# simulate data from a normal distribution
+array1 = np.random.normal(1.75, 0.20, 500)  #(mean, sd, n)
+
+# bind columns together
+np.columnstack((col1, col2))
+
 
 
 """ pandas objects/methods/functions  """ # =============================================================================
